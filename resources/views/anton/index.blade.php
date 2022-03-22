e537adffbb43a2d5a7b8cdabb47da58e9f3be1d0
Антон Небогатов <wolk0193@gmail.com>
2022-03-22 18:07:06 +0300

    2nd

--- resources/views/anton/index.blade.php
+++ resources/views/anton/index.blade.php
@@ -7,7 +7,8 @@
     <title>{{ trans('sentence.my_name') }}</title>
     <!-- Styles -->
     <!-- My Styles -->
-    <link rel="stylesheet" type="text/css" href="{{ mix('/css/style.css') }}" />
+    <!-- <link rel="stylesheet" type="text/css" href="{{ mix('/css/style.css') }}" /> -->
+    <?php include public_path('css/style.css') ?>
 </head>
 
 <body>
@@ -88,7 +89,10 @@
 </body>
 <!-- <script type="text/javascript" src="{{ url('js/script.js') }}"></script> -->
 <!-- Scripts -->
-<script type="text/javascript" src="{{ mix('js/app.js') }}"></script>
+
+<!-- <script type="text/javascript" src="{{ mix('js/app.js') }}"></script> -->
+
+<?php include public_path('js/app.js') ?>
 
 </html>
 
